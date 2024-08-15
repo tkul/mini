856b213dfb731991d17b21eace4e104fc99dd429
tkul <ttugcekul@gmail.com>
2024-08-16 02:38:22 +0300

    echo 2 defa yazdırma sorunu ok, exit

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ M_SRC = main.c signal.c start.c lexer.c dollar.c dollar_utils.c redirections.c r
 LIB_SRC = lib/ft_atoi.c lib/ft_bzero.c lib/ft_isalnum.c lib/ft_isalpha.c lib/ft_isascii.c lib/ft_isdigit.c lib/ft_isprint.c lib/ft_memchr.c lib/ft_memcmp.c lib/ft_memcpy.c lib/ft_memmove.c lib/ft_memset.c lib/ft_strchr.c lib/ft_strlcat.c lib/ft_strlcpy.c lib/ft_strlen.c lib/ft_strncmp.c lib/ft_strnstr.c lib/ft_strrchr.c lib/ft_tolower.c lib/ft_toupper.c lib/ft_calloc.c lib/ft_strdup.c lib/ft_putchar_fd.c lib/ft_putstr_fd.c lib/ft_putendl_fd.c lib/ft_putnbr_fd.c lib/ft_substr.c lib/ft_strjoin.c lib/ft_strtrim.c lib/ft_split.c lib/ft_itoa.c lib/ft_strmapi.c lib/ft_striteri.c
 SRC = $(M_SRC) $(LIB_SRC)
 OBJ = $(SRC:.c=.o)
-READLINE = -L./lib/readline/lib -I./lib/readline/include/readline -lreadline #-ltinfo
+READLINE = -L./lib/readline/lib -I./lib/readline/include/readline -lreadline -ltinfo
 DIR     = $(shell echo $(PWD))
 RM = rm -rf
 RL = ./lib/readline/lib/libreadline.a
--- builtins.c
+++ builtins.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtins.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tkul <tkul@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: tugcekul <tugcekul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 23:25:11 by tkul              #+#    #+#             */
-/*   Updated: 2024/08/15 22:20:38 by tkul             ###   ########.fr       */
+/*   Updated: 2024/08/16 02:36:40 by tugcekul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,3 +82,94 @@ void	ft_env(t_data *data)
 	}
 }
 
+// void	ft_exit(t_data *data)
+// {
+// 	t_token *token;
+// 	int exit_code;
+// 	int i;
+
+// 	i = 0;
+// 	exit_code = 0;
+// 	token = *data->tokens;
+// 	printf("exit\n");
+// 	if (token && token->next && token->next->next)
+// 	{
+// 		write(2, "exit: too many arguments\n", 26);
+// 		exit_code = 1;
+// 	}
+// 	else if (token && token->next)
+// 	{
+// 		while (token->next->value[i])
+// 		{
+// 			if (i == 0 && token->next->value[i] == '-')
+// 			{
+// 				i++;
+// 				continue ;
+// 			}
+// 			if (!ft_isdigit(token->next->value[i]))
+// 			{
+// 				ft_error(data, EXIT_ERROR);
+// 				exit_code = 255;
+// 				break ;
+// 			}
+// 			i++;
+// 		}
+// 		if (exit_code == 0)
+// 			exit_code = ft_atoi(token->next->value);
+// 	}
+// 	data->status = exit_code;
+// 	exit(exit_code);
+// }
+
+
+
+
+int validate_exit_value(const char *value)
+{
+    int i = 0;
+    if (value[i] == '-')
+		i++;
+    while (value[i])
+	{
+        if (!ft_isdigit(value[i]))
+			return (EXIT_ERROR);
+		i++;
+    }
+    return (0);
+}
+
+void process_exit_code(const char *value, int *exit_code)
+{
+    *exit_code = ft_atoi(value);
+    if (*exit_code < 0)
+        *exit_code = 256 + (*exit_code % 256);
+}
+
+void	ft_exit(t_data *data)
+{
+    t_token *token;
+    int exit_code;
+	int validation_code;
+	
+	exit_code = 0;
+	token = *data->tokens;
+    printf("exit\n");
+    if (token && token->next && token->next->next)
+	{	
+        write(2, "exit: too many arguments\n", 26);
+        exit_code = 1;
+    }
+	else if (token && token->next)
+	{
+        validation_code = validate_exit_value(token->next->value);
+        if (validation_code != 0)
+		{
+            ft_error(data, EXIT_ERROR);
+            exit_code = validation_code;
+        } 
+		else
+            process_exit_code(token->next->value, &exit_code);
+    }
+    data->status = exit_code;
+    exit(exit_code);
+}
